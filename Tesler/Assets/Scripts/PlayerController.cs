358e6ab4af5ab947188ca95abc32dfc21aa89d0b
Icyrotem <76842699+Icyrotem@users.noreply.github.com>
2025-06-18 00:19:10 +0300

    Sprites and movement
    I added Soldier and DOS and movement for the Soldier

---
+++ Tesler/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public int speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void FixedUpdate()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
+    }
+}
