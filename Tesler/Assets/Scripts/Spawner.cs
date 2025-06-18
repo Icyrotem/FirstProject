3f1d6f9d39442dc6928a40d5a44b6e918b2578d7
Icyrotem <76842699+Icyrotem@users.noreply.github.com>
2025-06-18 21:20:21 +0300

    Update Spawner.cs

--- Tesler/Assets/Scripts/Spawner.cs
+++ Tesler/Assets/Scripts/Spawner.cs
@@ -11,9 +11,9 @@ public class Spawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        random = Random.Range(-22.0f, 22.0f);
-        for (int i = 0; i < level; i++)
+        for (int i = 0; i < level+5; i++)
         {
+            random = Random.Range(-22.0f, 22.0f);
             Instantiate(enemy, new Vector3(random, -6.5f, 0), Quaternion.identity);
         }
     }
